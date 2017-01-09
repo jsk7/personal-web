135050be62c0cd28c17046df32b1d5b1f95b2f5b
Juan Ignacio Ostroski <juan.biltes@gmail.com>
2017-01-09 14:32:25 -0300

    Se actualiza HomePage con sombras y manos

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,10 +19,36 @@ const HomePage = (props) => {
       <div className="banner">
         <img className="profile" src="../icons/profile-size4-v2.jpg" />
       </div>
-      <Col lg={12}>
-        <h1>@futurejuan</h1>
-        <h4>Some some catchy phrase</h4>
+      <Col lg={1} md={1} sm={1} xsHidden></Col>
+      <Col lg={10} md={10} sm={10} xs={12}>
+      <div className="border left">
+      <div className="border top">
+      <div className="border right">
+        <Col lg={2} md={2} sm={2} xs={2}>
+          <div className="left">
+            <img className="hand" src="../icons/hand-leftside-v2.jpg" />
+          </div>
+        </Col>
+        <Col lg={8} md={8} sm={8} xs={8}>
+
+            <div className="central">
+              <h1>@futurejuan</h1>
+              <h4>Some some catchy phrase</h4>
+              <p>hola<br />s<br />a<br />c</p>
+            </div>
+        </Col>
+        <Col lg={2} md={2} sm={2} xs={2}>
+          <div className="right">
+            <img className="hand" src="../icons/hand-rightside-v2.jpg" />
+          </div>
+        </Col>
+        </div>
+        </div>
+        </div>
       </Col>
+      <Col lg={1} md={1} sm={1} xsHidden></Col>
+
+
     </div>
     </ReactCSSTransitionGroup>
   );
